0fe31c2c9afc04e88b60639b073f1d3192ddc303
arunkumarthangam22 <arunkumarthangam22@gmail.com>
2025-06-23 20:46:38 +0530

    removing comments in the code

--- backend/routes/invoiceRoutes.js
+++ backend/routes/invoiceRoutes.js
@@ -1,11 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const { createInvoice, getInvoices } = require('../controllers/invoiceController');
-
-// router.post('/invoices', createInvoice);
-// router.get('/invoices', getInvoices);
-
-// module.exports = router;
 
 
 const express = require('express');
